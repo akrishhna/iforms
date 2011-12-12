3b0dc4a3a362f88bf39035f056958e44b1113e8c
Ashwini Krishna <ashwini.patlola@gmail.com>
2011-12-12 01:53:23 -0600

    migration file cleanup

--- db/migrate/20110922211740_add_doctor_id_to_patient.rb
+++
@@ -1,9 +0,0 @@
-class AddDoctorIdToPatient < ActiveRecord::Migration
-  def self.up
-    add_column :patients, :doctor_id, :integer
-  end
-
-  def self.down
-    remove_column :patients, :doctor_id
-  end
-end
--- db/migrate/20110923005024_add_path_to_iform.rb
+++
@@ -1,9 +0,0 @@
-class AddPathToIform < ActiveRecord::Migration
-  def self.up
-    add_column :iforms, :path, :string
-  end
-
-  def self.down
-    remove_column :iforms, :path
-  end
-end
