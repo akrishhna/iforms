a9f227bdd4b55c9c6d29540cb4599fd3f5481d9e
Ashwini Krishna <ashwini.patlola@gmail.com>
2012-02-28 11:39:22 -0600

    changed urls in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Iforms::Application.configure do
     Configuration.pdftk_path = '/usr/bin/pdftk'
     Configuration.pdffiles_path = '/home/railsapps/iforms/current/public/pdffiles/'
     Configuration.csvfiles_path = '/home/railsapps/iforms/current/public/csvfiles/'
-    Configuration.notifier_path = 'http://ifor.ms'
+    Configuration.notifier_path = 'ifor.ms'
   end 
 
   
@@ -69,7 +69,7 @@ Iforms::Application.configure do
   
       config.action_view.debug_rjs             = true
    
-      config.action_mailer.default_url_options = { :host => 'http://ifor.ms' }
+      config.action_mailer.default_url_options = { :host => 'ifor.ms' }
       
       # Don't care if the mailer can't send
       config.action_mailer.raise_delivery_errors = false
