0ffbdfe3c373251f9a056cdc2c933f939a0540b3
Prasanna <prasofty@gmail.com>
2012-02-28 14:07:18 +0530

    added iform field update

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
 $(function() {
   $("#appointment_date").datepicker();
 
@@ -9,7 +11,6 @@ $(function() {
 $("#dd_dd").formToWizard({ submitButton: 'step_submit' });
 });
 
-
 //Check if the function exists
 if (typeof Element.insert !== "function") {
   //If not, hook it onto the $().append method.
@@ -17,3 +18,25 @@ if (typeof Element.insert !== "function") {
     $("#" + elem).append(ins.bottom);
   };
 }
+
+
+$(document).ready(function(){
+    $('form#dd_dd input, form#dd_dd select').change(function(){
+        var iform_id = $('#iform_id').val();
+        if(iform_id == '' || iform_id == undefined) {
+            return false;
+        }
+
+        var field = $(this).attr('name');
+        var field_val = $(this).val();
+        var params = {'iform_id':iform_id};
+        params[field] = field_val;
+
+        $.post('/iforms/iform_field_update',
+            params,
+            function(data){
+               console.log(data);
+            },
+        'json');
+    });
+});
