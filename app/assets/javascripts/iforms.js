e274c6d2a98094798e94ba376eeadcb29f01c8ed
Ranjith <ranjithnalimela@gmail.com>
2012-10-01 19:06:33 +0530

    fixed content of adult iform in dental tab

---
+++ app/assets/javascripts/iforms.js
@@ -0,0 +1,6 @@
+$('.adult_iforms_nav_link').live('click',function () {
+  console.log($(this).attr('data_tab_index'));
+  var tab_index = parseInt($(this).attr('data_tab_index'));
+  $("#adult_iforms_tab_links a:eq(" + tab_index + ")").tab('show');
+ // return false;
+});
