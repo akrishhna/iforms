d51aedaaa4b477297e6bb82965380ea5484d9eaa
Ranjith <ranjithnalimela@gmail.com>
2012-10-02 15:39:56 +0530

    added chaild form for dental tab

--- app/assets/javascripts/iforms.js
+++ app/assets/javascripts/iforms.js
@@ -4,3 +4,10 @@ $('.adult_iforms_nav_link').live('click',function () {
   $("#adult_iforms_tab_links a:eq(" + tab_index + ")").tab('show');
  // return false;
 });
+
+$('.chaild_iforms_nav_link').live('click',function () {
+  console.log($(this).attr('data_tab_index'));
+  var tab_index = parseInt($(this).attr('data_tab_index'));
+  $("#chaild_iforms_tab_links a:eq(" + tab_index + ")").tab('show');
+  // return false;
+});
