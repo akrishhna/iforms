7f32c1ce2344e0e86a89947253c2a3561e07c5a7
Ranjith <ranjithnalimela@gmail.com>
2012-10-09 17:50:28 +0530

    fixed new appointment form

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,21 +1,33 @@
-$(function(){
+$(function () {
 
-  if(params['appointment_id'] != undefined) {
-    $('#appointment_email').val(old_appointment['email']) ;
+  if (params['appointment_id'] != undefined) {
+    $('#appointment_email').val(old_appointment['email']);
     $('#appointment_firstname').val(old_appointment['firstname']);
     $('#appointment_lastname').val(old_appointment['lastname']);
     $('#appointment_responsible_party').val(old_appointment['responsible_party']);
-    $('[name="appointment[location]"][value="'+old_appointment['location']+'"]').attr('checked', 'checked');
+    $('[name="appointment[location]"][value="' + old_appointment['location'] + '"]').attr('checked', 'checked');
 
-   /* form checked in next appt
-   for(id in ids){
-      $('[name="form_ids[]"][value="'+ids[id]+'"]').attr('checked', 'checked');
-    }*/
+    /* form checked in next appt
+     for(id in ids){
+     $('[name="form_ids[]"][value="'+ids[id]+'"]').attr('checked', 'checked');
+     }*/
+  }
+
+  $(window).load(function () {
+    if (params.appointment != undefined) {
+      $('#appointment_date').datepicker('setDate', params.appointment["date"]);
+    }
+  });
+
+  if (params.form_ids != undefined) {
+    ids = params.form_ids
+    $(ids).each(function (k, v) {
+      $('.new_appointment input[value=' + v + ' ]').attr('checked', true);
+    });
   }
 
 
-
-  if($('div[data-page-controller=appointments][data-page-action=edit]').size()) {
+  if ($('div[data-page-controller=appointments][data-page-action=edit]').size()) {
     $(".appointments_date_selector").datepicker().datepicker("option", "dateFormat", "yy-mm-dd").datepicker('setDate', appointment["date"]);
   } else {
     $(".appointments_date_selector").datepicker().datepicker("option", "dateFormat", "yy-mm-dd").datepicker('setDate', params['appointment_date'] ? params['appointment_date'] : new Date());
