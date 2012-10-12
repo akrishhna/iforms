c10ef4ffeefb8b892aef71295eff927693eb0fb3
Ranjith <ranjithnalimela@gmail.com>
2012-10-12 11:01:26 +0530

    hide all tabs ,remove my appointments header , added well style

--- app/assets/javascripts/consumer.js
+++ app/assets/javascripts/consumer.js
@@ -7,12 +7,14 @@ $(function () {
 
   if ($('.page').attr('data-page') == "consumer_index") {
     $(window).load(function () {
-      if (consumer_tab_index != '' && consumer_tab_index != "0") {
+      if (consumer_tab_index != '' && consumer_tab_index != "1") {
         var tab_index = parseInt(consumer_tab_index);
         $("#consumer_tab_links a:eq(" + tab_index + ")").tab('show');
       } else {
-        $("#boy_scouts").attr('data-is_loaded', 'true');
-        $("#boy_scouts").load('/consumer/boy_scouts_list');
+        // $("#boy_scouts").attr('data-is_loaded', 'true');
+        //$("#boy_scouts").load('/consumer/boy_scouts_list');
+        $("#dental").attr('data-is_loaded', 'true');
+        $("#dental").load('/consumer/dental');
       }
     });
 
