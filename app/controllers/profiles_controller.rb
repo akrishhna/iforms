fb25eb2951df56398409a5ced19186aa91455e52
Ranjith <ranjithnalimela@gmail.com>
2012-10-30 10:33:46 +0530

    fixed profile issue for new sign up user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -21,17 +21,23 @@ class ProfilesController < ApplicationController
   end
 
   def edit
-    @profile = Profile.find(params[:id])
+    @profile = Profile.find(params[:id]) rescue nil
+    if @profile.nil?
+      @troop_leader_profile = GirlScoutTroopLeaderProfile.find(params[:id])
+      @profile = Profile.create(:user_id => current_user.id, :first_name => @troop_leader_profile.first_name, :last_name => @troop_leader_profile.last_name)
+    end
   end
 
   def update
    @profile = Profile.find(params[:id])
+   @troop_leader_profile = GirlScoutTroopLeaderProfile.find_by_user_id(@profile.user_id)
     if @profile.update_attributes(params[:profile])
       flash[:success] = 'Profile Updated successfully'
       service_provider = current_user.service_providers.first
       if service_provider.nil?
         redirect_to :controller => "consumer", :action => "index"
       elsif service_provider.id == 2
+        @troop_leader_profile.update_attributes(params[:profile])
         redirect_to girl_scouts_troop_leaders_path
       else
         redirect_to :controller => "consumer", :action => "index"
