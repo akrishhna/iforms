713dcc3ce70def00d530fcae5675ba8a76f71f0d
Ranjith <ranjithnalimela@gmail.com>
2012-10-30 15:56:55 +0530

    fixed sign up page page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -73,4 +73,8 @@ module ApplicationHelper
     will_paginate(pages, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => BootstrapLinkRenderer, :previous_label => '&larr;'.html_safe, :next_label => '&rarr;'.html_safe)
   end
 
-end
+  def title(page_title, show_title = true)
+    content_for(:title) { h(page_title.to_s) }
+    @show_title = show_title
+  end
+  end
