24b8ad3e279d3248c5adc376aa929eda2bab3425
Ranjith <ranjithnalimela@gmail.com>
2012-11-05 17:50:45 +0530

    added clear seach button

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,4 +1,9 @@
 $(function () {
 
  $('.add_admin_user_btn').fancybox();
+
+  $('.clear_admin_users_search').click(function () {
+    window.location.href = '/admin/admin_users';
+  });
+
 });
--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -1,1 +1,5 @@
-
+$(function () {
+  $('.clear_users_search').click(function () {
+     window.location.href = '/admin/users';
+  });
+});
