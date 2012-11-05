e498f478a86d87c3ef3dedeed9bb1f6c49e7b006
Ranjith <ranjithnalimela@gmail.com>
2012-11-05 10:04:05 +0530

    commemted uniqueness of email in roster

--- app/models/girls_scout.rb
+++ app/models/girls_scout.rb
@@ -2,5 +2,5 @@ class GirlsScout < ActiveRecord::Base
   belongs_to :user
   has_many :girl_scouts_activity_permission_forms
 
-validates_uniqueness_of :email, :scope => :service_provider_id
+#validates_uniqueness_of :email, :scope => :service_provider_id
 end
