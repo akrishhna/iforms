e23776ab53b4106b186bde3dc8102f3ebafd6a76
Ranjith <ranjithnalimela@gmail.com>
2012-11-06 12:45:48 +0530

    fixed user id in permission form

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,11 +21,11 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :lockable, :timeoutable and :activatable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :encryptable , :authentication_keys => [:username]
+         :encryptable, :authentication_keys => [:username]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :role, :edit_confirmed, :remember_me, :username
-  attr_accessible :service_provider_ids,:status
+  attr_accessible :service_provider_ids, :status
   attr_accessible :girl_scout_troop_leader_profile_attributes
   accepts_nested_attributes_for :girl_scout_troop_leader_profile
 
@@ -35,11 +35,12 @@ class User < ActiveRecord::Base
   def resolve_girl_scout
     @girl_scout = GirlsScout.find_by_email(self.email)
     if @girl_scout.present?
+      @user = User.find_by_email(@girl_scout.email)
       @girl_scouts_activity_permission_form = GirlScoutsActivityPermissionForm.find_all_by_girls_scout_id(@girl_scout.id)
-     @girl_scouts_activity_permission_form.each do |pf|
-       pf.user_id = @girl_scout.user_id if pf.user_id.nil?
-       pf.save
-     end
+      @girl_scouts_activity_permission_form.each do |pf|
+        pf.user_id = @user.id if pf.user_id.nil?
+        pf.save
+      end
     end
   end
 end
