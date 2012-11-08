5a700d33380a63ff9b21aa7265ff1ad54904e67a
Ranjith <ranjithnalimela@gmail.com>
2012-11-08 23:51:35 +0530

    added validation for mandatory fields

--- app/assets/javascripts/deviseroles.js
+++ app/assets/javascripts/deviseroles.js
@@ -1,4 +1,5 @@
 $(function () {
+
   $('.contact_us').change(function () {
     if ($('#name').val() != "" && $('#email').val() != "" && $('#subject').val() != "" && $('#message').val() != "") {
       $('.contact_us_submit').attr('disabled',false);
@@ -6,5 +7,13 @@ $(function () {
       $('.contact_us_submit').attr('disabled',"disabled");
     }
 
+  });
+
+  $('.girl_scout_sign_up_form').change(function () {
+    if ($('#user_username').val() != "" && $('#user_email').val() != "" && $('#user_password').val() != "" && $('#user_password_confirmation').val() != "") {
+      $('.girl_scout_sign_up_form_submit').attr('disabled',false);
+    }else{
+      $('.girl_scout_sign_up_form_submit').attr('disabled',"disabled");
+    }
   });
 });
