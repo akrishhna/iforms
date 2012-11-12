e0dd9bb0c5724956e893b52f47f8e93e74720769
Prasanna <prasofty@gmail.com>
2012-11-12 11:03:45 +0530

    merge to master

--- db/schema.rb
+++ db/schema.rb
@@ -13,26 +13,26 @@
 
 ActiveRecord::Schema.define(:version => 20121107090353) do
 
-  create_table "active_admin_comments", :force => true do |t|
-    t.string   "resource_id",   :null => false
-    t.string   "resource_type", :null => false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.text     "body"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.string   "namespace"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
-
   create_table "admin_users", :force => true do |t|
     t.integer  "user_id"
     t.boolean  "status",     :default => true
     t.datetime "created_at",                   :null => false
     t.datetime "updated_at",                   :null => false
+  end
+
+  create_table "admins", :force => true do |t|
+    t.string   "email",                             :default => "", :null => false
+    t.string   "encrypted_password", :limit => 128, :default => "", :null => false
+    t.integer  "sign_in_count",                     :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",                   :default => 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at",                                        :null => false
+    t.datetime "updated_at",                                        :null => false
   end
 
   create_table "appformjoins", :force => true do |t|
@@ -816,15 +816,6 @@ ActiveRecord::Schema.define(:version => 20121107090353) do
     t.datetime "updated_at",             :null => false
   end
 
-  create_table "static_data", :force => true do |t|
-    t.string   "name"
-    t.string   "value"
-    t.string   "data_type"
-    t.integer  "data_order"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "user_roles", :force => true do |t|
     t.integer  "user_id"
     t.integer  "role_id"
@@ -858,10 +849,10 @@ ActiveRecord::Schema.define(:version => 20121107090353) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                                   :null => false
     t.datetime "updated_at",                                                   :null => false
-    t.string   "password_salt"
     t.string   "role",                                  :default => "patient", :null => false
-    t.string   "username"
     t.string   "edit_confirmed"
+    t.string   "password_salt"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
