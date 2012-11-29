dcfa367ca540acaff84fb36f069775a7ecf74c04
Ranjith <ranjithnalimela@gmail.com>
2012-11-29 15:05:23 +0530

    changed name diamond activity state to state

--- app/models/static_data.rb
+++ app/models/static_data.rb
@@ -16,7 +16,7 @@ class StaticData < ActiveRecord::Base
     ["Daisy", "Brownie", "Junior", "Cadette", "Senior"]
   end
 
-  def self.diamond_activity_state
+  def self.state
     ['AL','AK','AS','AZ','AR','CA','CO','CT','DE','DC','FM','FL','GA','GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD',
      'MA','MI','MN','MS','MO''MT','NE','NV','NH','NJ','NM','NY','NC','ND','MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN',
      'TX','UT','VT','VI','VA','WA','WV','WI','WY']
