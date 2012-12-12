55208301d9b1979125515b61982ce68ea402f37e
Ranjith <ranjithnalimela@gmail.com>
2012-12-12 16:27:04 +0530

    setting date picker when resend

--- app/assets/javascripts/medical_appointments.js
+++ app/assets/javascripts/medical_appointments.js
@@ -1,10 +1,23 @@
 $(function () {
 
-  $(window).load(function () {
-    if (params.appointment != undefined) {
-      $('#appointment_date').datepicker('setDate', params.appointment["date"]);
-    }
+  $('.appointments_date_selector').datetimepicker({
+    controlType:'select',
+    timeFormat:'hh:mm tt'
   });
+
+  if ($('div[data-page-controller=medical_appointments][data-page-action=edit]').size()) {
+    var date_time = appointment["appointment_date_time"];
+    var date = date_time.split('T')[0];
+    var time_split = date_time.split('T')[1];
+    var time_h = time_split.split(':')[0];
+    var time_m = time_split.split(':')[1];
+    var app_date_time = date + ' ' + time_h + ':' + time_m + ' ';
+    console.log(app_date_time);
+    console.log('2000-01-01 12:41 ');
+    $(window).load(function () {
+      $("#medical_appointment_appointment_date_time").datetimepicker().datetimepicker("option", "dateFormat", "yy-mm-dd").datetimepicker('setDate', app_date_time);
+    });
+  }
 
   $(".appointments_date_selector").datepicker({
     onSelect:function (dateText, inst) {
@@ -19,16 +32,15 @@ $(function () {
     $('#medical_appointment_responsible_party').val(old_appointment['responsible_party']);
     $('#medical_appointment_preferred_name').val(old_appointment['preferred_name']);
     $('[name="medical_appointment[location]"][value="' + old_appointment['location'] + '"]').attr('checked', 'checked');
-
   } else {
-    if($('.page').attr('data-page') != 'medical_appointments_index' && $('.page').attr('data-page') != 'medical_appointments_new'){
-     if(appointment.formname.search(/New_Medical/) == 0) {
-      $('#radio_buttons_New_Medical_Patient').attr('checked', 'checked');
-    }else{
-      $('#radio_buttons_None').attr('checked', 'checked');
+    if ($('.page').attr('data-page') != 'medical_appointments_index' && $('.page').attr('data-page') != 'medical_appointments_new') {
+      if (appointment.formname.search(/New_Medical/) == 0) {
+        $('#radio_buttons_New_Medical_Patient').attr('checked', 'checked');
+      } else {
+        $('#radio_buttons_None').attr('checked', 'checked');
+      }
+    } else {
     }
-    }else{}
   }
-
 
 });
