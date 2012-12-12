5cbf62fd375bb40b08874eef29e72d104254b6dd
Ranjith <ranjithnalimela@gmail.com>
2012-12-12 10:50:01 +0530

    fixed boy scouts sign up page

--- app/assets/javascripts/deviseroles.js
+++ app/assets/javascripts/deviseroles.js
@@ -12,15 +12,30 @@ $(function () {
 
   $('.select_council_type_for_sign_up').change(function() {
     var value = $('.select_council_type_for_sign_up:checked').val();
-    $('.council_type_status.ok').hide();
-    $('.council_type_status.default').show();
-    $('#user_girl_scout_troop_leader_profile_attributes_council_type.default').val('');
     if(value == "boy_scouts") {
-      $('#user_girl_scout_troop_leader_profile_attributes_council_type').html(boy_scouts_council_options);
+      $('.council_type').hide();
+      $('.council_type_status.ok').hide();
+      $('.council_type_status.default').hide();
+      // $('#user_girl_scout_troop_leader_profile_attributes_council_type').html(boy_scouts_council_options);
     } else if (value == "girl_scouts") {
-      $('#user_girl_scout_troop_leader_profile_attributes_council_type').html(girl_scouts_council_options);
+      $('.council_type').show();
+      $('.council_type_status.ok').hide();
+      $('.council_type_status.default').show();
+      $('#user_girl_scout_troop_leader_profile_attributes_council_type').val('');
+     // $('#user_girl_scout_troop_leader_profile_attributes_council_type').html(girl_scouts_council_options);
     }
   });
 
+  $('#user_username, #user_email , #user_password, #user_password_confirmation').keyup(function(){
+    var params = $('#new_user').serializeArray();
+    $.post('/deviseroles/validate_girl_scout_sign_up_page',params );
+    return false;
+  });
+
+  $('select#user_girl_scout_troop_leader_profile_attributes_council_type').change(function(){
+    var params = $('#new_user').serializeArray();
+    $.post('/deviseroles/validate_girl_scout_sign_up_page',params );
+    return false;
+  });
   /* end Sign up page */
 });
