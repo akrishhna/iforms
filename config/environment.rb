8c8b110d073b85f83acada9578cc01dc7d50fad4
Ranjith <ranjithnalimela@gmail.com>
2012-12-31 17:56:22 +0530

    added constant paths

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ Iforms::Application.initialize!
 ENV['RECAPTCHA_PUBLIC_KEY'] = '6LdtBswSAAAAAPSp0BW9SJa7WkB5o1SVATcXCLf9'
 ENV['RECAPTCHA_PRIVATE_KEY'] = '6LdtBswSAAAAAOhw8Wtd1aHFEqAblE6cxrmosiXV'
 
+
+# Girl Scouts Paths
 CENTRAL_TEXAS_FORM_PATH = "#{PDFFILES_PATH}Central_Texas_Form.pdf"
 CENTRAL_TEXAS_READ_ONLY_FORM_PATH = "#{PDFFILES_PATH}Central_Texas_Read_Only.pdf"
 
@@ -16,6 +18,10 @@ DIAMONDS_READ_ONLY_FORM_PATH = "#{PDFFILES_PATH}Diamonds_Read_Only.pdf"
 ALASKA_FORM_PATH = "#{PDFFILES_PATH}Alaska_Form.pdf"
 ALASKA_READ_ONLY_PATH = "#{PDFFILES_PATH}Alaska_Read_Only.pdf"
 
+ARIZONA_CACTUS_PINE_FORM_PATH = "#{PDFFILES_PATH}Arizona_Cactus_Pine_Form.pdf"
+ARIZONA_CACTUS_PINE_READ_ONLY_PATH = "#{PDFFILES_PATH}Arizona_Cactus_Pine_Read_Only.pdf"
+
+# End  Girl Scouts Paths
 
 CAPITAL_MEDICAL_CLINIC_FORM_PATH = "#{PDFFILES_PATH}Capital_Medical_Clinic.pdf"
 CAPITAL_MEDICAL_CLINIC_READ_ONLY_FORM_PATH = "#{PDFFILES_PATH}Capital_Medical_Clinic_Read_Only.pdf"
