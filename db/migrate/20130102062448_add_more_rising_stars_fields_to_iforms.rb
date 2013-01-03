bb50bd9597f5414fa3505b40b1131af7723b73e9
Ranjith <ranjithnalimela@gmail.com>
2013-01-03 14:32:13 +0530

    changed field name

--- db/migrate/20130102062448_add_more_rising_stars_fields_to_iforms.rb
+++ db/migrate/20130102062448_add_more_rising_stars_fields_to_iforms.rb
@@ -3,7 +3,7 @@ class AddMoreRisingStarsFieldsToIforms < ActiveRecord::Migration
   def up
     add_column :iforms, :patient_guardian_father_address_type, :string, :limit => 10
     add_column :iforms, :patient_guardian_mother_address_type, :string, :limit => 10
-    add_column :iforms, :social_history_patient_adopted_age_year_months, :string, :limit => 10
+    add_column :iforms, :social_history_patient_adopted_age_years_months, :string, :limit => 10
   end
 
   def down
